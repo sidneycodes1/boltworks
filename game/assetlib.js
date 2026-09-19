8154563fb1a5e3372ba562e7894094c2e5ac776b
Junior Sydney <ngbedeworks@gmail.com>
2026-09-19 09:34:42 +0100

    chore(deploy): stamp 202609190834

--- game/assetlib.js
+++ game/assetlib.js
@@ -28,7 +28,7 @@
  */
 import * as THREE from 'three';
 import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
-import { applySurfaces } from './surfaces.js?v=202609190715';
+import { applySurfaces } from './surfaces.js?v=202609190834';
 
 const cache = new Map();   // url -> Promise<prototype>
 
--- game/src/main.js
+++ game/src/main.js
@@ -6,9 +6,9 @@
 console.log('[BOLTWORKS] main.js loaded');
 
 import * as THREE from 'three';
-import { ASSET } from '../assetlib.js?v=202609190715';
-import { setSurfaceDefaults } from '../surfaces.js?v=202609190715';
-import { BoltAudio } from './audio.js?v=202609190715';
+import { ASSET } from '../assetlib.js?v=202609190834';
+import { setSurfaceDefaults } from '../surfaces.js?v=202609190834';
+import { BoltAudio } from './audio.js?v=202609190834';
 
 const audio = new BoltAudio();
 
