cd5980217cc9985ba57f92c731b37569fabce754
Junior Sydney <ngbedeworks@gmail.com>
2026-09-19 12:17:53 +0100

    chore(deploy): stamp 202609191117

--- game/src/main.js
+++ game/src/main.js
@@ -6,9 +6,9 @@
 console.log('[BOLTWORKS] main.js loaded');
 
 import * as THREE from 'three';
-import { ASSET } from '../assetlib.js?v=202609190834';
-import { setSurfaceDefaults } from '../surfaces.js?v=202609190834';
-import { BoltAudio } from './audio.js?v=202609190834';
+import { ASSET } from '../assetlib.js?v=202609191117';
+import { setSurfaceDefaults } from '../surfaces.js?v=202609191117';
+import { BoltAudio } from './audio.js?v=202609191117';
 
 const audio = new BoltAudio();
 
