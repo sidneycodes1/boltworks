435c6dc392f07833b54d19fdfffbc84d9489fec4
Junior Sydney <ngbedeworks@gmail.com>
2026-09-20 16:59:56 +0100

    chore(deploy): stamp 202609201559

--- game/assetlib.js
+++ game/assetlib.js
@@ -28,7 +28,7 @@
  */
 import * as THREE from 'three';
 import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
-import { applySurfaces } from './surfaces.js?v=202609201507';
+import { applySurfaces } from './surfaces.js?v=202609201559';
 
 const cache = new Map();   // url -> Promise<prototype>
 
